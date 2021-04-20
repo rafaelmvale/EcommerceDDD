7d4f9598440d3224fd11390f8cfa6a280be000fe
Rafael Marques do Vale <rafael.marques.vale.dev@gmail.com>
2021-04-20 11:34:27 -0300

    projeto iniciado

---
+++ EcommerceDDD/Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,33 @@
+﻿using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConnectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+        
+        private string GetStringConnectionConfig()
+        {
+            string strCon = "Server=DESKTOP-GR6FASQ\\Rafael Vale;Database=DDD_ECOMMERCE;Trusted_Connection=True";
+            return strCon;
+        }
+    }
+}
---
+++ EcommerceDDD/Infrastructure/Repository/Generics/RepositoryGenerics.cs
@@ -0,0 +1,94 @@
+﻿using Domain.Interfaces.Generics;
+using Infrastructure.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Generics
+{
+    public class RepositoryGenerics<T> : IGeneric<T>, IDisposable where T : class
+    {
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+
+        public RepositoryGenerics()
+        {
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
+        }
+
+
+        public async Task Add(T objeto)
+        {
+            using(var data = new ContextBase(_OptionsBuilder))
+            {
+                await data.Set<T>().AddAsync(objeto);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete(T objeto)
+        {
+
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                data.Set<T>().Remove(objeto);
+                await data.SaveChangesAsync();
+            }
+        }
+
+
+        public async Task<T> GetEntityByid(int Id)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                return await data.Set<T>().FindAsync(Id);
+            }
+        }
+
+        public async Task<List<T>> List()
+        {
+
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                return await data.Set<T>().AsNoTracking().ToListAsync();
+            }
+        }
+
+        public async Task Update(T objeto)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                data.Set<T>().Update(objeto);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        #region Disposed https://docs.microsoft.com/pt-br/dotnet/standard/garbage-collection/implementing-dispose
+
+        bool disposed = false;
+
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) 
+                return;
+
+            if (disposing)
+            {
+                handle.Dispose();
+            }
+
+            disposed = true;
+
+        }
+        #endregion
+    }
+}
